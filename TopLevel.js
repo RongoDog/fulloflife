13c965a10b3e0896fec4448379957d60437faad7
Roni Peled <roni.peled1994@gmail.com>
2017-04-01 16:50:21 -0400

    Added HTML + JS

---
+++ TopLevel.js
@@ -0,0 +1,13 @@
+class Component_1 extends React.Component {
+
+    render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Componenet_1 />,
+    document.getElementById('main')
+);
